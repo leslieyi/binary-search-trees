230f579c53484371624c8b391edf03fb4f078a9d
leslieyi <leslieyi411@gmail.com>
2021-11-17 16:06:30 -0500

    insert and find

--- bst-class-practice.js
+++ bst-class-practice.js
@@ -10,42 +10,74 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+
   insert(value) {
     let newNode = new Node(value);
 
     if (this.root === null) {
       this.root = newNode;
       return this;
-    } else {
-      //other wise, is our value less than greater than the root?
-      //lessthan? is there a left? Looping
-      //greater than? is there a right?
-      let current = this.root;
-      while (true) {
-        if (value < current.value) {
-          if (current.left === null) {
-            current.left = newNode;
-            return this;
-          } else {
-            current = current.left;
-          }
-        } else if (value > current.value) {
-          if (current.right === null) {
-            current.right = newNode;
-            return this;
-          } else {
-            current = current.right;
-          }
+    }
+    //other wise, is our value less than greater than the root?
+    //lessthan? is there a left? Looping
+    //greater than? is there a right?
+    let current = this.root;
+    while (true) {
+      if (value === current.value) return undefined;
+      if (value < current.value) {
+        if (current.left === null) {
+          current.left = newNode;
+          return this;
         }
+        current = current.left;
+      } else {
+        if (current.right === null) {
+          current.right = newNode;
+          return this;
+        }
+        current = current.right;
       }
     }
   }
+
+  find(value) {
+    if (this.root === null) return false; //edge case
+    let current = this.root,
+      found = false;
+    while (current && !found) {
+      //when hit the end of the tree, there is no current===> the loop stops
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        found = true;
+      }
+    }
+    if (!found) return undefined; //false
+    return current;
+  }
+  contains(value) {
+    if (this.root === null) return false;
+    var current = this.root,
+      found = false;
+    while (current && !found) {
+      if (value < current.value) {
+        current = current.left;
+      } else if (value > current.value) {
+        current = current.right;
+      } else {
+        return true;
+      }
+    }
+    return false;
+  }
 }
 
-var tree = new BinarySearchTree();
-tree.insert(10)
-tree.insert(5)
-tree.insert(13)
-
-
+let tree = new BinarySearchTree();
+tree.insert(10);
+tree.insert(5);
+tree.insert(13);
 console.log(tree);
+console.log(tree.find(9));
+console.log(tree.find(5));
